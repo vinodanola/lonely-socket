eccaa93bd3997a4a296c4188e01017ad93f5b620
vinodanola <vino.danola@gmail.com>
2021-10-25 18:36:10 +0700

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const server = require('http').createServer();
 const io = require('socket.io')(server, {
   cors: {
-    origin: '*',
+    origin: 'https://react-3wtj9r.stackblitz.io',
     //path: '/socket',
   },
 });
